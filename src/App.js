e5e2507061dc99549c6244160b32ced4cc7032c5
crazefan <dolgopolovt@gmail.com>
2020-09-03 17:39:48 +0200

    protopype

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [config, setConfig] = useState({
+    items: [
+      { label: "number", type: "number" },
+      { label: "radio", type: "radio" },
+      { label: "text", type: "text" },
+      { label: "date", type: "date" },
+    ],
+  });
+
+  var bufferText = {};
+
+  const handleConfigInput = (e) => {
+    bufferText = e.target.value;
+  };
+  const handleApplyClick = () => {
+    setConfig(bufferText);
+  };
+
+  const renderFromConfig = () =>
+    config.items && typeof config.items !== "undefined" && config.length !== 0
+      ? config.items.map((item, i) =>
+          React.createElement("input", { type: item.type })
+        )
+      : null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ margin: "20px 50px" }}>
+      <input
+        type="textArea"
+        style={{ width: "200px", height: "200px" }}
+        onChange={handleConfigInput}
+      />
+      <button onClick={handleApplyClick}>APPLY</button>
+      {renderFromConfig()}
     </div>
   );
 }
-
 export default App;
